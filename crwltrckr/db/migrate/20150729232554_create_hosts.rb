81bfb864992b20f2749dab29f64c27c8523b37b4
Chip Jackson <chip@factual.com>
2015-07-30 17:50:13 -0700

    fix migration

--- crwltrckr/db/migrate/20150729232554_create_hosts.rb
+++ crwltrckr/db/migrate/20150729232554_create_hosts.rb
@@ -3,7 +3,6 @@ class CreateHosts < ActiveRecord::Migration
     create_table :hosts do |t|
       t.integer :pipelines_id
       t.string :domain
-      t.
 
       t.timestamps null: false
     end
