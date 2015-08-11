a635d6cbb8790e4018b6bceebeb9f133f61cd6d9
Chip Jackson <chip@factual.com>
2015-08-10 19:37:05 -0700

    Add button to refresh host stats

--- crwltrckr/app/controllers/hosts_controller.rb
+++ crwltrckr/app/controllers/hosts_controller.rb
@@ -4,4 +4,11 @@ class HostsController < ApplicationController
     @host.destroy
     redirect_to(:back)
   end
+
+  def refresh
+    @host = Host.find(params[:id])
+    @host.refresh_stats
+    @host.refresh_cached_ages
+    redirect_to(:back)
+  end
 end
--- crwltrckr/config/routes.rb
+++ crwltrckr/config/routes.rb
@@ -12,7 +12,11 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
   resources :crawl_projects
-  resources :hosts
+  resources :hosts do
+    member do
+      get 'refresh'
+    end
+  end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
